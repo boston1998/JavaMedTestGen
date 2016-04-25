4358abffb9344ff2bb91202f1054222104b0b548
Sergey Gruzdev <sergeygruzdev98@gmail.com>
2016-04-25 08:55:38 -0500

    Testing UDP pokes

---
+++ Student/app.js
@@ -0,0 +1,18 @@
+// Get required modules
+// Express
+// Socket
+// Net
+// FS (?)
+
+// My info
+
+// URL request processing
+
+// Set callback for the scanner
+
+// Set local callbacks for communicating with user control panel
+// Submit assignment - 
+
+
+// Listen
+// Open user control panel
